4f4b975d3db3662cd2877d28b72ac740da3d375e
ercolig-hub <75980081+ercolig-hub@users.noreply.github.com>
2021-04-28 09:24:01 +0200

    com

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,6 +1,7 @@
 const webpack = require("webpack");
 const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 const ESLintPlugin = require("eslint-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
 
 module.exports = {
   mode: "development",
@@ -11,6 +12,9 @@ module.exports = {
   },
   plugins: [
     new ReactRefreshWebpackPlugin(),
+    new Dotenv({
+      path: "./.env.development", // load this now instead of the ones in '.env'
+    }),
     new webpack.DefinePlugin({
       "process.env.name": JSON.stringify("Giucari"),
     }),
